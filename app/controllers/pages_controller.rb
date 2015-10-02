d0baf5188ab1472ef742ea3e0ada0daee1af11fa
dhruvilbadani <dhruvilbadani@gmail.com>
2015-10-01 20:22:26 -0800

    hw2

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,17 @@
+class PagesController < ApplicationController
+  def home
+    foo = Foobar.new "baz"
+    @baz = foo.bar :cat, sat: :dat, dat: :sat
+  end
+
+  def stringify
+    @text = "You are nothing!"
+  end
+
+  def age
+  end
+
+  def person
+    @per = Person.new(params[:name], params[:age])
+  end
+end
